ce8c528496b05794f304281befc488cefcb001a2
Developer1 <tovban.freelancer@gmail.com>
2023-02-06 20:32:35 +0700

    Add Vue example

---
+++ help.md
@@ -0,0 +1,21 @@
+# myvue
+
+## Setting up with Vue CLI
+```
+yarn global add @vue/cli
+```
+
+# create project
+```
+vue create myvue
+```
+
+# enter to project directory
+```
+cd myvue
+```
+
+# yarn serve to start build vue and run on web browser
+```
+yarn serve
+```
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div> {{ name }}</div>
+  <h2 v-if="num===0"> The number is zero</h2>
+  <h2 v-else-if="num<0"> The number is negative</h2>
+  <h2 v-else-if="num > 0"> The number is positive</h2>
+  <h2 v-else> Not a Number</h2>
+
+  <template v-if="display">
+    <h2>Sher</h2>
+    <h2>CodeVueJs</h2>
+    <h2>Vue</h2>
+  </template>
+
+  <h2 v-for="color in arrays" :key="color">{{ color }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name:"Hello World",
+      num:-3,
+      display: true,
+      arrays:['Red', 'Orange', 'Blue'],
+      fullNames:[{first:'Brue', last:'Lee'},{first:'Clark', last:'Ken'}, {first:'Princess', last:'Diana'}]
+    }
   }
 }
 </script>
@@ -22,5 +40,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+element.style{
+  display: none;
 }
 </style>
